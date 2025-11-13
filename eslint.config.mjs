bd63c5239f4b497fd1f010f60e06feb7845b792f
irfan jamil <hirfan@wynshop.com>
2025-11-14 04:41:40 +0500

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // ⛔ allow `any` everywhere
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
