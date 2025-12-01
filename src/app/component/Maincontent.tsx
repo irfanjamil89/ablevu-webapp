03d19fecd17d1577715415373ae291e66f08a585
Sajawal Jutt <mail.sajawaljutt@gmail.com>
2025-12-01 16:20:56 +0500

    Business Profile Sidebar integration

--- src/app/component/Maincontent.tsx
+++ src/app/component/Maincontent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import VirtualTour from './VirtualTour';
 
 interface MaincontentProps {
   businessId: string;
@@ -7,51 +8,7 @@ interface MaincontentProps {
 export default function Maincontent({ businessId }: MaincontentProps) {
   return (
     <div className='px-10 py-7 w-7/10'>
-      <div className="tour border p-6 rounded-3xl border-[#e5e5e7] w-full ">
-
-        <h3 className="text-xl font-[600] mb-4" >Virtual Tours</h3>
-        <p >Explore the location virtually to make informed decisions and plan your visit</p>
-        <p className='text-xs mt-1'>Interested in adding an accessibility virtual tour? Email <a href="mailto:info@ableeyes.org" ><span className="text-[#0205d3]">info@ableeyes.org</span></a> for more information</p>
-        <div className="tours mt-6 flex flex-wrap justify-between gap-3">
-          <div className="flex justify-between items-center border p-4 rounded-xl border-[#e5e5e7] w-[49%]">
-              <div className="icon flex items-center">
-                <img src="/assets/images/walking.svg" alt="" className="w-8 mr-4" />
-                <p>Enterance</p>
-              </div>
-              <div className="link">
-                <a href="https://maps.app.goo.gl/4bQdcAEkZk3fqpQH9">View</a>
-              </div>
-          </div>
-          <div className="flex justify-between items-center border p-4 rounded-xl border-[#e5e5e7] w-[49%]">
-              <div className="icon flex items-center">
-                <img src="/assets/images/walking.svg" alt="" className="w-8 mr-4" />
-                <p>Enterance</p>
-              </div>
-              <div className="link">
-                <a href="https://maps.app.goo.gl/4bQdcAEkZk3fqpQH9">View</a>
-              </div>
-          </div>
-          <div className="flex justify-between items-center border p-4 rounded-xl border-[#e5e5e7] w-[49%]">
-              <div className="icon flex items-center">
-                <img src="/assets/images/walking.svg" alt="" className="w-8 mr-4" />
-                <p>Enterance</p>
-              </div>
-              <div className="link">
-                <a href="https://maps.app.goo.gl/4bQdcAEkZk3fqpQH9">View</a>
-              </div>
-          </div>
-          <div className="flex justify-between items-center border p-4 rounded-xl border-[#e5e5e7] w-[49%]">
-              <div className="icon flex items-center">
-                <img src="/assets/images/walking.svg" alt="" className="w-8 mr-4" />
-                <p>Enterance</p>
-              </div>
-              <div className="link">
-                <a href="https://maps.app.goo.gl/4bQdcAEkZk3fqpQH9">View</a>
-              </div>
-          </div>
-        </div>
-      </div>
-
+      <VirtualTour/>
       <div className="audio my-8 border p-6 rounded-3xl border-[#e5e5e7] w-full">
         <h3 className="text-xl font-[600] mb-4" >Audio Tours</h3>
         <div className="audios border border-dotted p-10 rounded-xl border-[#e5e5e7] text-center flex flex-col justify-center items-center ">
