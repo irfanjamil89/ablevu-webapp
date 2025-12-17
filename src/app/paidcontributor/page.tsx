e5f72a5481bd112e0f34fc318fb377b6cf144a00
Sajawal Jutt <mail.sajawaljutt@gmail.com>
2025-12-18 02:30:09 +0500

    Cart and Payment Integration

--- src/app/paidcontributor/page.tsx
+++ src/app/paidcontributor/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export default function BecomeSeller() {
+  console.log("API BASE URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
   const start = async () => {
     const r1 = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}stripe/create-account`, {
       method: "POST",
@@ -7,7 +8,7 @@ export default function BecomeSeller() {
       body: JSON.stringify({ userId: "abc123", email: "seller@email.com" }),
     });
     const { url } = await r1.json();
-
+    console.log(url)
     window.location.href = url;
   };
 
